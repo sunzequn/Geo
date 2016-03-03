12e1102c7648ec6ead48d9333e3f71461766aade
sunzequn <sunzequn@live.cn>
2016-03-03 21:25:02 +0800

    merge

--- Data/src/main/java/com/sunzequn/geo/data/climate/pull/PlaceDetailMain.java
+++ Data/src/main/java/com/sunzequn/geo/data/climate/pull/PlaceDetailMain.java
@@ -1,9 +1,15 @@
 package com.sunzequn.geo.data.climate.pull;
 
 import com.sunzequn.geo.data.climate.bean.Place;
+import com.sunzequn.geo.data.climate.bean.PlaceDetail;
+import com.sunzequn.geo.data.climate.bean.PlaceWebWrapper;
 import com.sunzequn.geo.data.climate.dao.PlaceDao;
+import com.sunzequn.geo.data.climate.dao.PlaceDetailDao;
+import com.sunzequn.geo.data.climate.pull.parser.RegionParser;
 import com.sunzequn.geo.data.crawler.proxy.ProxyBean;
 import com.sunzequn.geo.data.crawler.proxy.ProxyHandler;
+import com.sunzequn.geo.data.geonames.crawler.HttpConnector;
+import com.sunzequn.geo.data.geonames.crawler.Response;
 
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -14,17 +20,47 @@ import java.util.Set;
  * Created by Sloriac on 16/3/3.
  */
 public class PlaceDetailMain {
-    private static final int THREAD_NUM = 60;
+    private static final int THREAD_NUM = 10;
     private static final int TIMEOUT = 7000;
-    private static final String PREFIX = "http://en.climate-data.org/";
-    private static final String SUFFIX = "/";
+    private static final String PREFIX = "http://en.climate-data.org";
     private static LinkedList<String> urls = null;
     private static ProxyHandler proxyHandler = new ProxyHandler();
+    private static PlaceDetailDao placeDetailDao = new PlaceDetailDao();
     private static PlaceDao placeDao = new PlaceDao("climate_seed_place");
     private static PlaceDao placeFromCountryDao = new PlaceDao("climate_seed_place_from_country");
 
     public static void main(String[] args) {
         initUrls();
+        for (int i = 0; i < THREAD_NUM; i++) {
+            new Thread(() -> {
+                ProxyBean proxy = getProxy();
+                while (true) {
+                    try {
+                        HttpConnector httpConnector = new HttpConnector();
+                        String url = getUri();
+                        if (url == null) {
+                            System.out.println(Thread.currentThread().getName() + "is over");
+                            return;
+                        }
+                        Response response = httpConnector.setUrl(url)
+                                .setProxy(proxy.getHost(), proxy.getPort())
+                                .getConnection().setTimeout(TIMEOUT).getContent();
+
+                        if (response.getCode() != 200) {
+                            addUrl(url);
+                            proxy = getProxy();
+                            continue;
+                        }
+                        String string = response.getContent().trim();
+                        PlaceDetail placeDetail = new PlaceDetail(url, string);
+                        savePlace(placeDetail);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }, "thread" + i).start();
+        }
     }
 
     private static synchronized ProxyBean getProxy() {
@@ -51,12 +87,16 @@ public class PlaceDetailMain {
 
     private static synchronized String getUri() {
         if (urls.size() > 0) {
-            return urls.pop();
+            return PREFIX + urls.pop();
         }
         return null;
     }
 
     private static synchronized void addUrl(String url) {
         urls.add(url);
+    }
+
+    private static synchronized void savePlace(PlaceDetail placeDetail) {
+        placeDetailDao.save(placeDetail);
     }
 }
